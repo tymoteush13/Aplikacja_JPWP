01b3406bd602a4b0419650a737e66189262be0fd
Tymek <tymo@student.agh.edu.pl>
2024-04-02 22:26:48 +0200

    first project

--- app/src/main/java/com/example/aplikacja/SecondPage.java
+++ app/src/main/java/com/example/aplikacja/SecondPage.java
@@ -1,11 +1,9 @@
 package com.example.aplikacja;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,29 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button button = findViewById(R.id.SecondActivitybutton);
+        Button button = findViewById(R.id.prev_button);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SecondPage.class);
+                Intent intent = new Intent(SecondPage.this,MainActivity.class);
                 startActivity(intent);
             }
 
         });
     }
-
-
-
 }
