f66d483643450741e8bff2959ab9b271e5992fba
Tymek <tymo@student.agh.edu.pl>
2024-04-06 15:02:43 +0200

    splash_screen

--- app/src/main/java/com/example/aplikacja/MainActivity.java
+++ app/src/main/java/com/example/aplikacja/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.aplikacja;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.activity.EdgeToEdge;
@@ -14,10 +15,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView showCountTextView;
+    Button count_button;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Aplikacja);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -34,6 +38,26 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+           Button toast = findViewById(R.id.toast_button);
+        toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(MainActivity.this, "Hello toast!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+        showCountTextView = findViewById(R.id.numbers);
+        count_button = findViewById(R.id.count_button);
+        count_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                showCountTextView.setText(String.format(getString(R.string.clicked_d_times), count));
+            }
+        });
+
+
+
     }
 
 
